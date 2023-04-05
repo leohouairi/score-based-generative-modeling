53918c20e8e554f5d6e0598514e90b8208fd8a77
cthiounn <thiounn.conrad@gmail.com>
2023-04-05 11:30:37 +0000

    fix name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='score_based_generative_modeling',
     packages=find_packages(),
     version='0.1.0',
     description='Generative model a la Yang Song',
